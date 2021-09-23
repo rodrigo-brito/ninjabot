890e053be72aea34058374a314a39ee28fca3bf4
Rodrigo Brito <rodrigo-brito@users.noreply.github.com>
2021-09-23 19:00:06 -0300

    temp

--- examples/strategies/emacross.go
+++ examples/strategies/emacross.go
@@ -24,17 +24,13 @@ func (e CrossEMA) Indicators(df *ninjabot.Dataframe) {
 
 func (e *CrossEMA) OnCandle(df *ninjabot.Dataframe, broker service.Broker) {
 	closePrice := df.Close.Last(0)
-	log.Info("New Candle = ", df.Pair, df.LastUpdate, closePrice)
-
 	assetPosition, quotePosition, err := broker.Position(df.Pair)
 	if err != nil {
 		log.Error(err)
 	}
 
-	buyAmount := 4000.0
-	if quotePosition > buyAmount && df.Close.Crossover(df.Metadata["ema9"]) {
-		size := buyAmount / closePrice
-		_, err := broker.CreateOrderMarket(ninjabot.SideTypeBuy, df.Pair, size)
+	if quotePosition > 10 && df.Close.Crossover(df.Metadata["ema9"]) {
+		_, err := broker.CreateOrderMarketQuote(ninjabot.SideTypeBuy, df.Pair, quotePosition/2)
 		if err != nil {
 			log.WithFields(map[string]interface{}{
 				"pair":  df.Pair,
@@ -42,12 +38,11 @@ func (e *CrossEMA) OnCandle(df *ninjabot.Dataframe, broker service.Broker) {
 				"close": closePrice,
 				"asset": assetPosition,
 				"quote": quotePosition,
-				"size":  size,
 			}).Error(err)
 		}
 	}
 
-	if assetPosition*closePrice > 10 && // minimum tradable size
+	if assetPosition > 0 &&
 		df.Close.Crossunder(df.Metadata["ema9"]) {
 		_, err := broker.CreateOrderMarket(ninjabot.SideTypeSell, df.Pair, assetPosition)
 		if err != nil {
