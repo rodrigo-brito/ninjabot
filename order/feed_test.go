4e772ed247f6b72d90eadac561ead373ec6e696f
Léo Viggiano <53899920+theviggo@users.noreply.github.com>
2021-10-01 15:07:50 -0300

    feat(test): add tests for order feed (#28)

---
+++ order/feed_test.go
@@ -0,0 +1,26 @@
+package order
+
+import (
+	"testing"
+
+	"github.com/rodrigo-brito/ninjabot/model"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFeed_NewOrderFeed(t *testing.T) {
+	feed := NewOrderFeed()
+	require.NotEmpty(t, feed)
+}
+
+func TestFeed_Subscribe(t *testing.T) {
+	feed, symbol := NewOrderFeed(), "blaus"
+	called := make(chan bool, 1)
+
+	feed.Subscribe(symbol, func(order model.Order) {
+		called <- true
+	}, false)
+
+	feed.Start()
+	feed.Publish(model.Order{Symbol: symbol}, false)
+	require.True(t, <-called)
+}
