e52124f353e6858aa478fbeb75e7f4c4567546d9
Rodrigo Brito <rodrigo-brito@users.noreply.github.com>
2021-10-09 00:39:37 -0300

    refactor(storage): simplify storage functions

--- storage/storage_test.go
+++ storage/storage_test.go
@@ -13,7 +13,7 @@ func TestNewBunt(t *testing.T) {
 	repo, err := FromMemory()
 	require.NoError(t, err)
 
-	err = repo.CreateOrder(&model.Order{
+	firstOrder := &model.Order{
 		ExchangeID: 1,
 		Symbol:     "BTCUSDT",
 		Side:       model.SideTypeBuy,
@@ -23,10 +23,11 @@ func TestNewBunt(t *testing.T) {
 		Quantity:   1,
 		CreatedAt:  now.Add(-time.Minute),
 		UpdatedAt:  now.Add(-time.Minute),
-	})
+	}
+	err = repo.CreateOrder(firstOrder)
 	require.NoError(t, err)
 
-	err = repo.CreateOrder(&model.Order{
+	secondOrder := &model.Order{
 		ExchangeID: 2,
 		Symbol:     "BTCUSDT",
 		Side:       model.SideTypeBuy,
@@ -36,28 +37,35 @@ func TestNewBunt(t *testing.T) {
 		Quantity:   1,
 		CreatedAt:  now.Add(time.Minute),
 		UpdatedAt:  now.Add(time.Minute),
-	})
+	}
+	err = repo.CreateOrder(secondOrder)
 	require.NoError(t, err)
 
-	t.Run("pending order", func(t *testing.T) {
-		pendings, err := repo.GetPendingOrders()
-		require.NoError(t, err)
-		require.Len(t, pendings, 1)
-		require.Equal(t, model.OrderStatusTypeNew, pendings[0].Status)
-	})
-
 	t.Run("filter with date restriction", func(t *testing.T) {
-		orders, err := repo.Filter(WithUpdateAtBeforeOrEqual(now))
+		orders, err := repo.Orders(WithUpdateAtBeforeOrEqual(now))
 		require.NoError(t, err)
 		require.Len(t, orders, 1)
 		require.Equal(t, orders[0].ExchangeID, int64(1))
 	})
 
 	t.Run("get all", func(t *testing.T) {
-		orders, err := repo.Filter()
+		orders, err := repo.Orders()
 		require.NoError(t, err)
 		require.Len(t, orders, 2)
 		require.Equal(t, orders[0].ExchangeID, int64(1))
 		require.Equal(t, orders[1].ExchangeID, int64(2))
+	})
+
+	t.Run("update", func(t *testing.T) {
+		firstOrder.Status = model.OrderStatusTypeCanceled
+		err := repo.UpdateOrder(firstOrder)
+		require.NoError(t, err)
+
+		orders, err := repo.Orders(WithStatus(model.OrderStatusTypeCanceled))
+		require.NoError(t, err)
+		require.Len(t, orders, 1)
+		require.Equal(t, firstOrder.ID, orders[0].ID)
+		require.Equal(t, firstOrder.Price, orders[0].Price)
+		require.Equal(t, firstOrder.Quantity, orders[0].Quantity)
 	})
 }
