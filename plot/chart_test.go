bfdeb24d00c4f1be5258ad5b22496b428d4c81c6
Panapol Poungdech <boom.rst28@gmail.com>
2022-01-18 23:36:05 +0700

    [test] candle and order

--- plot/chart_test.go
+++ plot/chart_test.go
@@ -10,18 +10,20 @@ import (
 )
 
 func TestChart_CandleAndOrder(t *testing.T) {
+	pair := "ETHUSDT"
 	c, err := NewChart()
 	require.NoErrorf(t, err, "error when initial chart")
 
+	//feed candle and order 1
 	candle := model.Candle{
-		Pair:     "BTCUSDT",
-		Time:     time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
-		Open:     10000.1,
-		Close:    10000.1,
-		Low:      10000.1,
-		High:     10000.1,
-		Volume:   10000.1,
-		Trades:   10000,
+		Pair:     "ETHUSDT",
+		Time:     time.Date(2021, 9, 26, 20, 0, 0, 0, time.UTC),
+		Open:     3057.67,
+		Close:    3059.37,
+		Low:      3011.00,
+		High:     3115.51,
+		Volume:   87666.8,
+		Trades:   205069,
 		Complete: true,
 	}
 	c.OnCandle(candle)
@@ -29,14 +31,14 @@ func TestChart_CandleAndOrder(t *testing.T) {
 	order := model.Order{
 		ID:         1,
 		ExchangeID: 1,
-		Pair:       "BTCUSDT",
-		Side:       "SELL",
+		Pair:       "ETHUSDT",
+		Side:       "BUY",
 		Type:       "MARKET",
 		Status:     "FILLED",
-		Price:      10000.1,
-		Quantity:   1,
-		CreatedAt:  time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
-		UpdatedAt:  time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+		Price:      3059.37,
+		Quantity:   1.634323,
+		CreatedAt:  time.Date(2021, 9, 26, 20, 0, 0, 0, time.UTC),
+		UpdatedAt:  time.Date(2021, 9, 26, 20, 0, 0, 0, time.UTC),
 		Stop:       nil,
 		GroupID:    nil,
 		RefPrice:   0,
@@ -44,34 +46,155 @@ func TestChart_CandleAndOrder(t *testing.T) {
 		Candle:     model.Candle{},
 	}
 	c.OnOrder(order)
-
 	expectOrder := Order{
 		ID:        1,
-		Side:      "SELL",
+		Side:      "BUY",
 		Type:      "MARKET",
 		Status:    "FILLED",
-		Price:     10000.1,
-		Quantity:  1,
-		CreatedAt: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
-		UpdatedAt: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+		Price:     3059.37,
+		Quantity:  1.634323,
+		CreatedAt: time.Date(2021, 9, 26, 20, 0, 0, 0, time.UTC),
+		UpdatedAt: time.Date(2021, 9, 26, 20, 0, 0, 0, time.UTC),
+		RefPrice:  0,
 	}
 	require.Equal(t, &expectOrder, c.orderByID[order.ID])
 
+	//feed candle and oco order
+	candle2 := model.Candle{
+		Pair:     "ETHUSDT",
+		Time:     time.Date(2021, 9, 28, 8, 0, 0, 0, time.UTC),
+		Open:     2894.18,
+		Close:    2926.80,
+		Low:      2876.12,
+		High:     2940.74,
+		Volume:   88470.1,
+		Trades:   161354,
+		Complete: true,
+	}
+	c.OnCandle(candle2)
+
+	groupID := int64(3)
+	limitMakerOrder := model.Order{
+		ID:         3,
+		ExchangeID: 3,
+		CreatedAt:  time.Date(2021, 9, 28, 8, 0, 0, 0, time.UTC),
+		UpdatedAt:  time.Date(2021, 9, 28, 8, 0, 0, 0, time.UTC),
+		Pair:       pair,
+		Side:       "SELL",
+		Type:       model.OrderTypeLimitMaker,
+		Status:     model.OrderStatusTypeNew,
+		Price:      2926.00,
+		Quantity:   1.634323,
+		GroupID:    &groupID,
+		RefPrice:   3059.37,
+	}
+	c.OnOrder(limitMakerOrder)
+	expectLimitMakerOrder := Order{
+		ID:        3,
+		Side:      "SELL",
+		Type:      "LIMIT_MAKER",
+		Status:    "NEW",
+		Price:     2926,
+		Quantity:  1.634323,
+		CreatedAt: time.Date(2021, 9, 28, 8, 0, 0, 0, time.UTC),
+		UpdatedAt: time.Date(2021, 9, 28, 8, 0, 0, 0, time.UTC),
+		OCOGroup:  &groupID,
+		RefPrice:  3059.37,
+	}
+	require.Equal(t, &expectLimitMakerOrder, c.orderByID[limitMakerOrder.ID])
+
+	stop := 2900.00
+	stopOrder := model.Order{
+		ID:         4,
+		ExchangeID: 3,
+		CreatedAt:  time.Date(2021, 9, 28, 8, 0, 0, 0, time.UTC),
+		UpdatedAt:  time.Date(2021, 9, 28, 8, 0, 0, 0, time.UTC),
+		Pair:       pair,
+		Side:       "SELL",
+		Type:       model.OrderTypeStopLoss,
+		Status:     model.OrderStatusTypeNew,
+		Price:      3000.00,
+		Stop:       &stop,
+		Quantity:   1.634323,
+		GroupID:    &groupID,
+		RefPrice:   3059.37,
+	}
+	c.OnOrder(stopOrder)
+	expectStopOrder := Order{
+		ID:        4,
+		Side:      "SELL",
+		Type:      "STOP_LOSS",
+		Status:    "NEW",
+		Price:     3000,
+		Quantity:  1.634323,
+		CreatedAt: time.Date(2021, 9, 28, 8, 0, 0, 0, time.UTC),
+		UpdatedAt: time.Date(2021, 9, 28, 8, 0, 0, 0, time.UTC),
+		Stop:      &stop,
+		OCOGroup:  &groupID,
+		RefPrice:  3059.37,
+	}
+	require.Equal(t, &expectStopOrder, c.orderByID[stopOrder.ID])
+
+	//test candles by pair
 	expectCandleByPair := []Candle{
 		{
-			Time:   time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
-			Open:   10000.1,
-			Close:  10000.1,
-			High:   10000.1,
-			Low:    10000.1,
-			Volume: 10000.1,
+			Time:   time.Date(2021, 9, 26, 20, 0, 0, 0, time.UTC),
+			Open:   3057.67,
+			Close:  3059.37,
+			High:   3115.51,
+			Low:    3011.00,
+			Volume: 87666.8,
 			Orders: []Order{
 				expectOrder,
 			},
 		},
+		{
+			Time:   time.Date(2021, 9, 28, 8, 0, 0, 0, time.UTC),
+			Open:   2894.18,
+			Close:  2926.80,
+			High:   2940.74,
+			Low:    2876.12,
+			Volume: 88470.1,
+			Orders: []Order{
+				expectLimitMakerOrder,
+				expectStopOrder,
+			},
+		},
 	}
-	actual := c.candlesByPair(candle.Pair)
-	require.Equal(t, expectCandleByPair, actual)
+	candles := c.candlesByPair(pair)
+	require.Equal(t, expectCandleByPair, candles)
+
+	//test shapes by pare
+	expectShapesByPair := []Shape{
+		{
+			StartX: time.Date(2021, 9, 28, 8, 0, 0, 0, time.UTC),
+			EndX:   time.Date(2021, 9, 28, 8, 0, 0, 0, time.UTC),
+			StartY: 3059.37,
+			EndY:   2926,
+			Color:  "rgba(0, 255, 0, 0.3)",
+		},
+		{
+			StartX: time.Date(2021, 9, 28, 8, 0, 0, 0, time.UTC),
+			EndX:   time.Date(2021, 9, 28, 8, 0, 0, 0, time.UTC),
+			StartY: 3059.37,
+			EndY:   3000,
+			Color:  "rgba(255, 0, 0, 0.3)",
+		},
+	}
+	shaped := c.shapesByPair(pair)
+	require.Equal(t, expectShapesByPair, shaped)
+
+	//test equity value by pair
+	expectAsset := []assetValue{}
+	expectQuote := []assetValue{}
+	asset, quote := c.equityValuesByPair(pair)
+	require.Equal(t, expectAsset, asset)
+	require.Equal(t, expectQuote, quote)
+
+	//test indicator by pair
+	expectIndicators := []plotIndicator{}
+	indicators := c.indicatorsByPair(pair)
+	require.Equal(t, expectIndicators, indicators)
 }
 
 func TestChart_WithPort(t *testing.T) {
